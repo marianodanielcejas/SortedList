8e6bb35b60c589ff0a93ef4b190619def8fae15d
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2022-10-16 21:06:14 -0300

    Agregar archivos de proyecto.

---
+++ SortedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            SortedList<string, string> Diccionario = new SortedList<string, string>();
+
+            Diccionario.Add("hello", "hola");
+            Diccionario.Add("good bye", "adios");
+            Diccionario.Add("I am", "yo soy");
+            Diccionario.Add("beutiful", "hermoso");
+            Diccionario.Add("farher", "padre");
+            Diccionario.Add("brother", "hermano");
+            Diccionario.Add("dog", "perro");
+            Diccionario.Add("cat", "gato");
+
+            Console.WriteLine("Ingresa una frase en ingles");
+            frase = Console.ReadLine();
+
+            if (Diccionario.ContainsKey(frase))
+            {
+                Console.WriteLine($"La frase {frase}, se traduce como: {Diccionario[frase]}");
+            }
+            else
+            {
+                Console.WriteLine($"No se ha encontrado la frase '{frase}' en el Diccionario");
+            }
+
+            //consola mantenga abierta
+            Console.ReadKey();
+        }
+    }
+}
